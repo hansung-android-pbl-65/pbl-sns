fe4c8619b41cbf3dcf03d572cc8a9496fb0bf5c3
MolruException <molruexception@gmail.com>
2022-11-19 07:22:47 +0900

    Feat: Post 클래스 구조 추상화 제거

--- app/src/main/java/com/androidpbl/pblsns/post/posts/Reply.kt
+++ app/src/main/java/com/androidpbl/pblsns/post/posts/Reply.kt
@@ -1,6 +1,17 @@
 package com.androidpbl.pblsns.post.posts
 
-class Reply constructor(user: Int) : AbstractPost(user) {
+import com.androidpbl.pblsns.post.PostStatus
+import java.time.ZonedDateTime
 
+class Reply constructor(_user: Int) {
+
+    // 게시글 / 댓글 기본 정보
+    private val user: Int = _user;
+    private var status: PostStatus = PostStatus.POST
+    private var reply: String = ""
+
+    // 게시글 / 댓글 상태 정보
+    private var isEdited: Boolean = false;
+    private lateinit var date: ZonedDateTime
 
 }
