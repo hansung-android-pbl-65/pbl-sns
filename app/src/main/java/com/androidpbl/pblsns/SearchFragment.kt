4208dba03d414c96b65dbef1b4a3d922d32a085f
hyeonwoo <ziczin7176@gmail.com>
2022-11-20 15:46:56 +0900

    Feat: 검색결과 자동 refresh, submit 후 소프트키 숨기기
    검색어와 데이터 비교 후recyclerview 자동 refresh, submit 후 자동으로
    소프트키 숨기기

--- app/src/main/java/com/androidpbl/pblsns/SearchFragment.kt
+++ app/src/main/java/com/androidpbl/pblsns/SearchFragment.kt
@@ -1,14 +1,15 @@
 package com.androidpbl.pblsns
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.widget.SearchView
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.androidpbl.pblsns.databinding.FragmentSearchBinding
@@ -19,9 +20,10 @@ import com.google.firebase.ktx.Firebase
 
 class SearchFragment : Fragment() {
     // 파이어베이스 구조 정해지면 수정
-    //val db: FirebaseFirestore = Firebase.firestore
-    //val rootCollectionReference = db.collection("/root")
-    private val datas = mutableListOf<String>()
+    private val db: FirebaseFirestore = Firebase.firestore
+    private val rootCollectionReference = db.collection("/root")
+    private var itemList = mutableListOf<String>()
+    private lateinit var adapter: MyAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,65 +33,83 @@ class SearchFragment : Fragment() {
         val binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
         val searchView = binding.searchView
 
-
-        var temp: String? = ""
         searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
             override fun onQueryTextChange(newText: String?): Boolean {
-                temp = newText
+                filterList(newText)
                 return true
             }
 
             override fun onQueryTextSubmit(query: String?): Boolean {
-//                파이어베이스 구조 정해지면 수정
-//                rootCollectionReference.get().addOnSuccessListener {
-//                    for(doc in it){
-//                        if(doc.id.contains(temp!!)) {
-////                            datas.clear()
-//                            datas.add(doc.id)
-//                        }
-//                    }
-//                }
+                searchView.clearFocus()
                 return true
             }
         })
 
-        // 리사이클러뷰 테스트
-        for(i in 1..9){
-            datas.add("index $i")
+        val layoutManager = LinearLayoutManager(activity)
+        binding.recyclerView.clearFocus()
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager = layoutManager
+
+        // collection 에서 documents 불러오기
+        rootCollectionReference.get().addOnSuccessListener {
+            // 각 document id를 add
+            for(doc in it){
+                itemList.add(doc.id)
+            }
+            // document id를 담은 itemList를 Adapter에 전달
+            adapter = MyAdapter(itemList)
+            binding.recyclerView.adapter = adapter
+        }.addOnFailureListener {
+            Toast.makeText(activity, "Get From FireStore Is Failed", Toast.LENGTH_SHORT).show()
         }
 
-        val adapter = MyAdapter(datas)
-        val layoutManager = LinearLayoutManager(activity)
-//        binding.recyclerView.clearFocus()
+        // 자동으로 검색창에 포커스 및 소프트키보드 올라오기
+        val popUpSoftKeyboard =  View.OnFocusChangeListener { view, hasFocus ->
+            val imm : InputMethodManager = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
-        binding.recyclerView.layoutManager = layoutManager
-        binding.recyclerView.adapter = adapter
-        binding.recyclerView.setHasFixedSize(true)
+            if (hasFocus) {
+                val isShowed = imm.showSoftInput(view.findFocus(), InputMethodManager.SHOW_IMPLICIT)
+                Log.d("focus"," ## hasFocus -> $hasFocus , isShowing -> $isShowed")        // both are true!
+            }
+        }
+        searchView.setOnQueryTextFocusChangeListener(popUpSoftKeyboard)
 
         return binding.root
-    }
+    }// onCreateView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentSearchBinding.bind(view)
-    }
+    }// onViewCreated
 
+    fun filterList(text: String?){
+        var filteredList = mutableListOf<String>()
+        for(item in itemList){
+            if(item.lowercase().contains(text!!.lowercase())){
+                // 입력받은 text를 item이 포함한다면
+                filteredList.add(item)
+            }
+        }
 
-    fun refreshFragment(fragment: Fragment, fragmentManager: FragmentManager){
-        var ft: FragmentTransaction = fragmentManager.beginTransaction()
-        ft.detach(fragment).attach(fragment).commit()
-    }
-
-
-
-
+        if(filteredList.isEmpty()){
+            Toast.makeText(activity, "No Data Found", Toast.LENGTH_SHORT).show()
+        }else{
+            adapter.setFilteredList(filteredList)
+        }
+    }// filteList
 
     class MyViewHolder(val binding: ItemRecyclerviewBinding): RecyclerView.ViewHolder(binding.root)
 
-    class MyAdapter(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    class MyAdapter(var itemList: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+        fun setFilteredList(filteredList: MutableList<String>){
+            this.itemList = filteredList
+            notifyDataSetChanged()
+        }
+
         override fun getItemCount(): Int {
             // 항목의 갯수 반환
-            return datas.size
+            return itemList.size
         }
 
         // single expression으로 viewHolder 객체를 반환
@@ -101,7 +121,8 @@ class SearchFragment : Fragment() {
         // 두번째 인자 : getItemCount에서 반환받은 데이터 인덱스
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
             val binding = (holder as MyViewHolder).binding
-            binding.itemData.text = datas[position]
+            // 뷰 텍스트에 데이터 대입
+            binding.itemData.text = itemList[position]
 
             binding.itemRoot.setOnClickListener {
                 // 태그 검색결과 -> 해당 short Post 목록 프래그먼트로 이동
@@ -109,5 +130,5 @@ class SearchFragment : Fragment() {
                 Log.d("test", "clicked view ${position+1}")
             }
         }
-    }
+    }// MyAdapter
 }
