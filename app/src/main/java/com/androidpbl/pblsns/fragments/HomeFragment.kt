7c3f4c39266f0cf2ee746ca077e0cac63d4cb2d0
MolruException <molruexception@gmail.com>
2022-11-26 18:53:30 +0900

    Feat: 홈 프래그먼트에 Post 배치

--- app/src/main/java/com/androidpbl/pblsns/fragments/HomeFragment.kt
+++ app/src/main/java/com/androidpbl/pblsns/fragments/HomeFragment.kt
@@ -1,56 +1,117 @@
 package com.androidpbl.pblsns.fragments
 
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.ListView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.androidpbl.pblsns.R
+import com.androidpbl.pblsns.databinding.FragmentHomeBinding
+import com.androidpbl.pblsns.databinding.ItemRecyclerviewBinding
+import com.androidpbl.pblsns.databinding.PostShortLayoutBinding
 import com.androidpbl.pblsns.event.EventHandler
 import com.androidpbl.pblsns.event.EventManager
 import com.androidpbl.pblsns.event.Listener
 import com.androidpbl.pblsns.post.PostManager
 import com.androidpbl.pblsns.post.events.PostFindEvent
+import com.androidpbl.pblsns.post.posts.Post
+import com.google.common.collect.Lists
+import kotlin.coroutines.coroutineContext
 
-class HomeFragment : Fragment(), Listener {
+class HomeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        EventManager.registerListener(this);
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
-        PostManager.findLatestPost(10)
-        return view;
-    }
+    ): View {
+        val binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        val posts = mutableListOf<Post>();
 
-    @EventHandler
-    fun onPostFind(event: PostFindEvent) {
-        val posts = event.posts;
+        val view = binding.postShort
+        view.clearFocus()
+        view.setHasFixedSize(true);
+        view.layoutManager = LinearLayoutManager(activity)
 
-        val adapter = view?.findViewById<ListView>(R.id.listview_post_short)
+        PostManager.collection.get().addOnSuccessListener {
+            posts.addAll(it.toObjects(Post::class.java))
+            view.adapter = SimplePostAdapter(posts);
+        }.addOnFailureListener {
 
+            // 테스트 포스트 추가
+            posts.add(Post("test_user_1", "포스트 테스트"))
+            posts.add(Post("test_user_2", "포스트 테스트"))
+            posts.add(Post("test_user_3", "포스트 테스트"))
+            posts.add(Post("test_user_4", "포스트 테스트"))
+            posts.add(Post("test_user_4", "포스트 테스트 1 \n포스트 테스트 2"))
+            posts.add(Post("test_user_4", "포스트 테스트 1 \n포스트 테스트 2 \n포스트 테스트 3"))
+
+            view.adapter = SimplePostAdapter(posts);
+            Toast.makeText(activity, "Firestore 연결 오류", Toast.LENGTH_SHORT).show()
+        }
+
+        return binding.root;
     }
 
     companion object {
 
         const val NAME = "홈"
 
-        @JvmStatic
-        fun newInstance() =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
+    }
 
-                }
+    class SimplePostViewHolder(val binding: PostShortLayoutBinding) : RecyclerView.ViewHolder(binding.root)
+
+    class SimplePostAdapter constructor(var posts: MutableList<Post>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val binding = PostShortLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return SimplePostViewHolder(binding);
+        }
+
+        override fun getItemCount(): Int {
+            return posts.size;
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val binding = (holder as SimplePostViewHolder).binding
+            val post = posts[position]
+
+            //TODO: 유저 썸네일 바인딩
+            binding.userThumb.setImageResource(R.drawable.ic_baseline_person_24)
+
+            // 유저 이름, 포스트 바인딩
+            binding.userName.text = post.user;
+            binding.postShortContent.text = post.post;
+
+            // 팔로우 이벤트 바인딩
+            binding.followButton.setOnClickListener {
+                //TODO: 클릭 상태에 따라 팔로우 추가/해제 기능 작동
+                Log.d("test", "follow button click")
             }
 
+            // 댓글 확인 바인딩
+            binding.commentButton.setOnClickListener {
+                //TODO: full 포스트 확인 페이지 이동
+                Log.d("test", "comment button click")
+            }
+
+            // 좋아요 클릭 바인딩
+            binding.likeButton.setOnClickListener {
+                //TODO: 좋아요 추가/해제 기능 작동
+                Log.d("test", "like button click")
+            }
+
+            Log.d("test", "user = ${post.user}, post = ${post.post}")
+        }
+
     }
+
 }
--- app/src/main/java/com/androidpbl/pblsns/post/posts/Post.kt
+++ app/src/main/java/com/androidpbl/pblsns/post/posts/Post.kt
@@ -4,19 +4,19 @@ import com.androidpbl.pblsns.post.PostManager
 import com.androidpbl.pblsns.post.PostStatus
 import java.time.ZonedDateTime
 
-class Post constructor(_user: Int) {
+class Post constructor(_user: String, _post: String) {
 
     // 게시글 / 댓글 기본 정보
-    private val user: Int = _user;
-    private var status: PostStatus = PostStatus.POST
-    private var post: String = ""
+    val user: String = _user;
+    var status: PostStatus = PostStatus.POST
+    var post: String = _post
 
     // 게시글 / 댓글 상태 정보
-    private var isEdited: Boolean = false;
-    private lateinit var date: ZonedDateTime
+    var isEdited: Boolean = false;
+    lateinit var date: ZonedDateTime
 
     // 댓글 / 리플 정보
-    private lateinit var comments: MutableList<Comment>
+    lateinit var comments: MutableList<Comment>
 
     fun upload() {
         PostManager.upload(this)
