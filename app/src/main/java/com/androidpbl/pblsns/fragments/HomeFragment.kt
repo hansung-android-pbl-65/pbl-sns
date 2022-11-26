ffa35ad3f930c9effb7c1144b01c495e0cd9f4fc
MolruException <molruexception@gmail.com>
2022-11-26 19:44:27 +0900

    Feat: 스크롤을 완전히 내릴경우 게시물을 추가로 불러올 수 있도록 설정

--- app/src/main/java/com/androidpbl/pblsns/fragments/HomeFragment.kt
+++ app/src/main/java/com/androidpbl/pblsns/fragments/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.androidpbl.pblsns.fragments
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -14,6 +15,7 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.OnScrollListener
 import com.androidpbl.pblsns.R
 import com.androidpbl.pblsns.databinding.FragmentHomeBinding
 import com.androidpbl.pblsns.databinding.ItemRecyclerviewBinding
@@ -27,20 +29,22 @@ import com.androidpbl.pblsns.post.posts.Post
 import com.google.common.collect.Lists
 import kotlin.coroutines.coroutineContext
 
-class HomeFragment : Fragment() {
+class HomeFragment constructor(private var posts: MutableList<Post>) : Fragment() {
+
+    constructor() : this(mutableListOf())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
-        val posts = mutableListOf<Post>();
 
         val view = binding.postShort
         view.clearFocus()
         view.setHasFixedSize(true);
         view.layoutManager = LinearLayoutManager(activity)
 
+        //TODO: 최신 게시물들을 가져오도록 할 것
         PostManager.collection.get().addOnSuccessListener {
             posts.addAll(it.toObjects(Post::class.java))
             view.adapter = SimplePostAdapter(posts);
@@ -58,7 +62,37 @@ class HomeFragment : Fragment() {
             Toast.makeText(activity, "Firestore 연결 오류", Toast.LENGTH_SHORT).show()
         }
 
+        binding.postShort.addOnScrollListener(object: OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (scrollPercent(binding.postShort) >= 100) {
+                    Toast.makeText(activity, "새로운 게시물을 불러오는 중입니다", Toast.LENGTH_SHORT).show()
+
+                    val startIndex = posts.size
+
+                    //TODO: 최신 게시물들을 가져오도록 할 것
+                    PostManager.collection.get().addOnSuccessListener {
+                        posts.addAll(it.toObjects(Post::class.java))
+                        view.adapter?.notifyItemRangeInserted(startIndex, posts.size - startIndex)
+                    }.addOnFailureListener {
+
+                        // 테스트 포스트 추가
+                        posts.add(Post("addition_user", "포스트 테스트"))
+                        posts.add(Post("addition_user", "포스트 테스트 1 \n포스트 테스트 2"))
+                        posts.add(Post("addition_user", "포스트 테스트 1 \n포스트 테스트 2 \n포스트 테스트 3"))
+
+                        view.adapter?.notifyItemRangeInserted(startIndex, 3)
+                        Toast.makeText(activity, "Firestore 연결 오류", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        })
+
         return binding.root;
+    }
+
+    fun scrollPercent(view: RecyclerView): Double {
+        return (view.computeVerticalScrollOffset() * 1.0 / (view.computeVerticalScrollRange() - view.computeVerticalScrollExtent())) * 100.0
     }
 
     companion object {
@@ -83,13 +117,17 @@ class HomeFragment : Fragment() {
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
             val binding = (holder as SimplePostViewHolder).binding
             val post = posts[position]
+            val blackColor = Color.parseColor("#000000")
 
             //TODO: 유저 썸네일 바인딩
             binding.userThumb.setImageResource(R.drawable.ic_baseline_person_24)
 
             // 유저 이름, 포스트 바인딩
             binding.userName.text = post.user;
+            binding.userName.setTextColor(blackColor)
+
             binding.postShortContent.text = post.post;
+            binding.postShortContent.setTextColor(blackColor)
 
             // 팔로우 이벤트 바인딩
             binding.followButton.setOnClickListener {
