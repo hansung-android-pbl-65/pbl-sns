c659f2bf283924683f3bf940eb1b070ab15aee81
kyw <96057775+blueberrycoffee@users.noreply.github.com>
2022-11-26 19:00:44 +0900

    FollowerActivty Editing
    리얼타임 db -> firestore db로 변경중

--- app/src/main/java/com/androidpbl/pblsns/FollowerActivity.java
+++ app/src/main/java/com/androidpbl/pblsns/FollowerActivity.java
@@ -8,11 +8,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.util.Log;
 
+//firestore db
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+/*firestore db 아닌 realtime db 여서 삭제
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+*/
 
 import java.util.ArrayList;
 
@@ -22,8 +30,8 @@ public class FollowerActivity extends AppCompatActivity {
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
     private ArrayList<FollowerInfo> arrayList;
-    private FirebaseDatabase database;
-    private DatabaseReference databaseReference;
+    private FirebaseFirestore database;
+    private DocumentReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,24 +44,25 @@ public class FollowerActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         arrayList=new ArrayList<>(); //팔로우를 담은 리스트
 
-        database=FirebaseDatabase.getInstance(); //파이어베이스 DB 연결
-        databaseReference=database.getReference("kyw");
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+        database=FirebaseFirestore.getInstance(); //파이어스토어 DB 연결
+        databaseReference=database.collection("kyw").document(); //컬렉션에서 문서가져옴
+        databaseReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            public void onSuccess(@NonNull DocumentSnapshot dataSnapshot) {
                 //파이어베이스 DB받아오는 곳
                 arrayList.clear();
-                for(DataSnapshot snapshot:dataSnapshot.getChildren()){ //데이터 list추출
-                FollowerInfo user = snapshot.getValue(FollowerInfo.class);
-                arrayList.add(user);
+
+               for(DocumentSnapshot snapshot:dataSnapshot.get()){ //데이터 list추출해야하는데 오류
+                    FollowerInfo user = snapshot.toObject(FollowerInfo.class);
+                    arrayList.add(user);
             }
                 adapter.notifyDataSetChanged();
             }
 
-            @Override
+            /*@Override
             public void onCancelled(@NonNull DatabaseError error) { //에러처리
-
             }
+            */
         });
 
         adapter = new FollowerAdapter(arrayList, this);
