56a3bb98587adbff14a3c82b368ce2fbad103077
kyw <96057775+blueberrycoffee@users.noreply.github.com>
2022-11-27 19:31:26 +0900

    dd
    dd

--- app/src/main/java/com/androidpbl/pblsns/FollowerActivity.java
+++ app/src/main/java/com/androidpbl/pblsns/FollowerActivity.java
@@ -13,6 +13,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 /*firestore db 아닌 realtime db 여서 삭제
 import com.google.firebase.database.DataSnapshot;
@@ -45,25 +46,38 @@ public class FollowerActivity extends AppCompatActivity {
         arrayList=new ArrayList<>(); //팔로우를 담은 리스트
 
         database=FirebaseFirestore.getInstance(); //파이어스토어 DB 연결
-        databaseReference=database.collection("kyw").document(); //컬렉션에서 문서가져옴
-        databaseReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(@NonNull DocumentSnapshot dataSnapshot) {
-                //파이어베이스 DB받아오는 곳
-                arrayList.clear();
-
-               for(DocumentSnapshot snapshot:dataSnapshot.get()){ //데이터 list추출해야하는데 오류
-                    FollowerInfo user = snapshot.toObject(FollowerInfo.class);
-                    arrayList.add(user);
+        //databaseReference=database.collection("kyw").get(); //컬렉션에서 문서가져옴
+        database.collection("kyw").get().addOnSuccessListener(runnable -> {
+            for (QueryDocumentSnapshot queryDocumentSnapshot : runnable) {
+                FollowerInfo user = queryDocumentSnapshot.toObject(FollowerInfo.class);
+                arrayList.add(user);
             }
-                adapter.notifyDataSetChanged();
-            }
+            adapter.notifyDataSetChanged();
+        }
+        );
 
-            /*@Override
+//        /*@Override
+//        databaseReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//            public void onSuccess(@NonNull DocumentSnapshot dataSnapshot) {
+//                //파이어베이스 DB받아오는 곳
+//                arrayList.clear();
+//                /*
+//                for(DataSnapshot snapshot:dataSnapshot.getChildren()){ //데이터 list추출
+//                    FollowerInfo user = snapshot.getValue(FollowerInfo.class);
+//                    arrayList.add(user); <-realtime database 형식임
+//                    */
+//               for(DocumentSnapshot snapshot:dataSnapshot){ //데이터 list추출해야하는데 오류
+//                    FollowerInfo user = dataSnapshot.toObject(FollowerInfo.class);
+//                    arrayList.add(user);
+//            }
+//                adapter.notifyDataSetChanged();
+//            }
+//
+
+           /* @Override
             public void onCancelled(@NonNull DatabaseError error) { //에러처리
-            }
-            */
-        });
+            }*/
+       // });
 
         adapter = new FollowerAdapter(arrayList, this);
         recyclerView.setAdapter(adapter);
